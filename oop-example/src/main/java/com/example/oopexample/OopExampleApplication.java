34c2a105f24b3e3e61ce65411b673ef799347011
limwoobin <drogba02@naver.com>
2021-11-27 16:19:14 +0900

    oop example project init

---
+++ oop-example/src/main/java/com/example/oopexample/OopExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopExampleApplication.class, args);
+    }
+
+}
---
+++ oop-example/src/test/java/com/example/oopexample/OopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
