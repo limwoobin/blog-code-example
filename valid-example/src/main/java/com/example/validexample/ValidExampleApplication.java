19929a63ee912861d8fb7b6d65735c271c00cb3b
limwoobin <drogba02@naver.com>
2022-01-08 18:26:17 +0900

    add - valid example project init

---
+++ valid-example/src/main/java/com/example/validexample/ValidExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.validexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidExampleApplication.class, args);
+    }
+
+}
---
+++ valid-example/src/test/java/com/example/validexample/ValidExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
