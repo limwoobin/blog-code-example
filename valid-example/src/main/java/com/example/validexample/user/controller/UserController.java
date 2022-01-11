96a57d48c7ca1766da5ab35ade555ab54f0590bf
limwoobin <drogba02@naver.com>
2022-01-11 20:30:23 +0900

    add - valid , advice 예제코드 추가

--- valid-example/src/main/java/com/example/validexample/user/controller/UserController.java
+++ valid-example/src/main/java/com/example/validexample/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.validexample.user.controller;
 
 import com.example.validexample.user.domain.UserDTO;
+import com.example.validexample.user.domain.UserRequest;
 import com.example.validexample.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,7 +18,11 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping(value = "")
-    public ResponseEntity<UserDTO> create() {
+    public ResponseEntity create(@Valid @RequestBody UserRequest userRequest , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors() , HttpStatus.BAD_REQUEST);
+        }
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- valid-example/src/main/java/com/example/validexample/user/domain/UserRequest.java
+++ valid-example/src/main/java/com/example/validexample/user/domain/UserRequest.java
@@ -20,7 +20,15 @@ public class UserRequest {
     @Size(min = 2 , max = 5 , message = "이름은 2자 이상 , 5자 이하여야 합니다.")
     private String name;
 
-    @NotBlank(message = "나이를 입력해주세요.")
-    @Size(min = 20 , max = 100 , message = "나이는 20~100세 사이의 사용자만 가입이 가능합니다.")
-    private int age;
+    @Min(value = 20 , message = "나이는 20~100세 사이의 사용자만 가입이 가능합니다.")
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
