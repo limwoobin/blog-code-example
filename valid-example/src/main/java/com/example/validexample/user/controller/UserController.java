b48ec6fd62fb0bbe6cddb7d0f0589c5f6e29316f
limwoobin <drogba02@naver.com>
2022-01-19 20:53:01 +0900

    add - BindResult test code 작성

--- valid-example/src/main/java/com/example/validexample/user/controller/UserController.java
+++ valid-example/src/main/java/com/example/validexample/user/controller/UserController.java
@@ -3,16 +3,20 @@ package com.example.validexample.user.controller;
 import com.example.validexample.user.domain.UserDTO;
 import com.example.validexample.user.domain.UserRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @GetMapping(value = "")
@@ -27,14 +31,26 @@ public class UserController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-//    @GetMapping(value = "")
-//    public ResponseEntity create(@Valid UserRequest userRequest , BindingResult bindingResult) {
-//        if (bindingResult.hasErrors()) {
-//            return new ResponseEntity<>(bindingResult.getAllErrors() , HttpStatus.BAD_REQUEST);
-//        }
-//
-//        return new ResponseEntity<>(HttpStatus.CREATED);
-//    }
+    @GetMapping(value = "/v2")
+    public ResponseEntity create(@Valid UserRequest userRequest , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> list = bindingResult.getFieldErrors();
+            for(FieldError error : list) {
+                return new ResponseEntity<>(error.getDefaultMessage() , HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/v2")
+    public ResponseEntity createForPost(@Valid @RequestBody UserRequest userRequest , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors() , HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> getUser(@Valid @PathVariable Long id) {
--- valid-example/src/test/java/com/example/validexample/ValidTest.java
+++ valid-example/src/test/java/com/example/validexample/ValidTest.java
@@ -4,6 +4,7 @@ import com.example.validexample.user.controller.UserController;
 import com.example.validexample.user.domain.UserRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -22,90 +24,111 @@ public class ValidTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Test
-    @DisplayName("Valid 조건에 맞지 않는 파라미터를 Get으로 넘기면 실패해야 한다")
-    void validTest_get() throws Exception {
-        // given
-        UserRequest userRequest = UserRequest.builder()
-                .email("drogba02")
-                .name("woobeen")
-                .age(29)
-                .build();
+    @Nested
+    @DisplayName("기본 Valid 테스트")
+    class DefaultValidTest {
 
-        // then
-        mockMvc.perform(get("/user")
-                .param("email" , userRequest.getEmail())
-                .param("name" , userRequest.getName())
-                .param("age" , Integer.toString(userRequest.getAge())))
-                .andExpect(status().isBadRequest());
+        @Test
+        @DisplayName("Valid 조건에 맞지 않는 파라미터를 Get으로 넘기면 실패해야 한다")
+        void validTest_get() throws Exception {
+            // given
+            UserRequest userRequest = UserRequest.builder()
+                    .email("drogba02")
+                    .name("woobeen")
+                    .age(29)
+                    .build();
+
+            // then
+            mockMvc.perform(get("/user")
+                    .param("email" , userRequest.getEmail())
+                    .param("name" , userRequest.getName())
+                    .param("age" , Integer.toString(userRequest.getAge())))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Valid 조건에 맞는 파라미터를 Get으로 넘기면 성공해야 한다")
+        void validTest2_get() throws Exception {
+            // given
+            UserRequest userRequest = UserRequest.builder()
+                    .email("drogba02@naver.com")
+                    .name("woobeen")
+                    .age(29)
+                    .build();
+
+            // then
+            mockMvc.perform(get("/user")
+                    .param("email" , userRequest.getEmail())
+                    .param("name" , userRequest.getName())
+                    .param("age" , Integer.toString(userRequest.getAge())))
+                    .andExpect(status().isCreated());
+        }
+
+        @Test
+        @DisplayName("Valid 조건에 맞지 않는 파라미터를 Post로 넘기면 실패해야 한다")
+        void validTest_post() throws Exception {
+            // given
+            UserRequest userRequest = UserRequest.builder()
+                    .email("drogba02")
+                    .name("woobeen")
+                    .age(29)
+                    .build();
+
+            String jsonData = objectMapper.writeValueAsString(userRequest);
+
+            // then
+            mockMvc.perform(post("/user")
+                    .content(jsonData)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Valid 조건에 맞는 파라미터를 Post로 넘기면 성공해야 한다")
+        void validTest2_post() throws Exception {
+            // given
+            UserRequest userRequest = UserRequest.builder()
+                    .email("drogba02@naver.com")
+                    .name("woobeen")
+                    .age(29)
+                    .build();
+
+            String jsonData = objectMapper.writeValueAsString(userRequest);
+
+            // then
+            mockMvc.perform(post("/user")
+                    .content(jsonData)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isCreated());
+        }
     }
 
-    @Test
-    @DisplayName("Valid 조건에 맞는 파라미터를 Get으로 넘기면 성공해야 한다")
-    void validTest2_get() throws Exception {
-        // given
-        UserRequest userRequest = UserRequest.builder()
-                .email("drogba02@naver.com")
-                .name("woobeen")
-                .age(29)
-                .build();
+    @Nested
+    @DisplayName("BindResult Valid 테스트")
+    class BindResultValidTest {
 
-        // then
-        mockMvc.perform(get("/user")
-                        .param("email" , userRequest.getEmail())
-                        .param("name" , userRequest.getName())
-                        .param("age" , Integer.toString(userRequest.getAge())))
-                .andExpect(status().isCreated());
-    }
+        static final String EXPECTED_EMAIL_ERR_MESSAGE = "이메일 형식이 맞지 않습니다.";
 
-    @Test
-    @DisplayName("Valid 조건에 맞지 않는 파라미터를 Post로 넘기면 실패해야 한다")
-    void validTest_post() throws Exception {
-        // given
-        UserRequest userRequest = UserRequest.builder()
-                .email("drogba02")
-                .name("woobeen")
-                .age(29)
-                .build();
+        @Test
+        @DisplayName("Valid 조건에 맞지 않는 파라미터를 Get으로 넘기면 실패하고 에러메시지가 찍혀야한다")
+        void validTest_get() throws Exception {
+            // given
+            UserRequest userRequest = UserRequest.builder()
+                    .email("drogba02")
+                    .name("woobeen")
+                    .age(29)
+                    .build();
 
-        String jsonData = objectMapper.writeValueAsString(userRequest);
 
-        // then
-        mockMvc.perform(post("/user")
-                .content(jsonData)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Valid 조건에 맞는 파라미터를 Post로 넘기면 성공해야 한다")
-    void validTest2_post() throws Exception {
-        // given
-        UserRequest userRequest = UserRequest.builder()
-                .email("drogba02@naver.com")
-                .name("woobeen")
-                .age(29)
-                .build();
-
-        String jsonData = objectMapper.writeValueAsString(userRequest);
-
-        // then
-        mockMvc.perform(post("/user")
-                .content(jsonData)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+            // then
+            mockMvc.perform(get("/user/v2")
+                    .param("email" , userRequest.getEmail())
+                    .param("name" , userRequest.getName())
+                    .param("age" , Integer.toString(userRequest.getAge())))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().string(EXPECTED_EMAIL_ERR_MESSAGE));
+        }
     }
 }
-
-//    Member member = Member.builder().name("Tom").build();
-//    Gson gson = new Gson();
-//    String content = gson.toJson(member);
-//
-//        mvc.perform(post("/api/member")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(content))
-//                .andExpect(status().isCreated());
-//
-//                verify(memberService).insert(member);
