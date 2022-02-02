5255c8a9c99652fa8f99c4e4ba27bcb7249f5f6c
limwoobin <drogba02@naver.com>
2022-02-02 19:26:35 +0900

    add - mapstruct example project init

---
+++ mapstruct-example/src/main/java/com/example/mapstructexample/MapstructExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapstructexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructExampleApplication.class, args);
+    }
+
+}
