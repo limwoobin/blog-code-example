fcb060a54ff0703d00e48e172652e36ddf3444b8
limwoobin <drogba02@naver.com>
2022-02-20 18:51:07 +0900

    add - mapper test code #4

--- mapstruct-example/src/main/java/com/example/mapstructexample/application/mapper/UserMapper.java
+++ mapstruct-example/src/main/java/com/example/mapstructexample/application/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package com.example.mapstructexample.application.mapper;
 import com.example.mapstructexample.application.controller.UserDTO;
 import com.example.mapstructexample.application.domain.Address;
 import com.example.mapstructexample.application.domain.User;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 /*
 * 롬복 최신버전은 annotationProcessor "org.projectlombok:lombok:1.18.6" 없이 된다
@@ -30,4 +34,13 @@ public interface UserMapper {
 
     @Mapping(target = "addressDTO" , source = "address")
     UserDTO toUserDTO_v7(User user , Address address);
+
+    UserDTO toUserDTO_v_9(User user);
+
+    @Mapping(target = "name" , ignore = true)
+    @Named("v10")
+    UserDTO toUserDTO_v10(User user);
+
+    @IterableMapping(qualifiedByName = "v10")
+    List<UserDTO> toDTOList(List<User> users);
 }
