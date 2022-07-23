ff5aea4778b1e7bcb263d8bac03dcb823175cfb3
limwoobin <drogba02@naver.com>
2022-07-23 17:42:29 +0900

    feat: LockExample Application Init

---
+++ lock-example/src/main/java/com/example/lockexample/LockExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockExampleApplication.class, args);
+    }
+
+}
---
+++ lock-example/src/test/java/com/example/lockexample/LockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
