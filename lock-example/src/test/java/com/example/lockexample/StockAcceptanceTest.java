e3133ba876ad17020ccdf343e433af7953f730dd
limwoobin <drogba02@naver.com>
2022-07-26 03:40:54 +0900

    test: 낙관적락 동시성 인수테스트 작성

--- lock-example/src/test/java/com/example/lockexample/StockAcceptanceTest.java
+++ lock-example/src/test/java/com/example/lockexample/StockAcceptanceTest.java
@@ -1,13 +1,18 @@
 package com.example.lockexample;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.example.lockexample.ui.StockRequest;
 import com.example.lockexample.ui.StockResponse;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.concurrent.CountDownLatch;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,31 +31,34 @@ public class StockAcceptanceTest {
     }
 
     @Test
-    void 재고를_차감한다() {
+    void 동시에_재고를_차감한다() throws InterruptedException, ExecutionException {
         StockResponse 재고 = N개의_재고를_생성(1L).as(StockResponse.class);
-        N개의_재고를_차감(재고.getId(), 1L);
-    }
 
-    @Test
-    void 동시에_재고를_차감한다() throws InterruptedException {
-        StockResponse 재고 =  N개의_재고를_생성(1L).as(StockResponse.class);
-
-        int numberOfThreads = 2;
+        int numberOfThreads = 4;
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
-        CountDownLatch latch = new CountDownLatch(numberOfThreads);
 
-        for (int i=0; i<numberOfThreads; i++) {
-            executorService.execute(() -> {
-                N개의_재고를_차감(재고.getId(), 1L);
-                latch.countDown();
-            });
+        int failedCount = 0;
+        List<Future<?>> list = new ArrayList<>();
+        for (int i = 0; i < numberOfThreads; i++) {
+            Future<?> task = executorService.submit(() -> N개의_재고를_차감(재고.getId(), 1L));
+            list.add(task);
         }
 
-        latch.await();
+        for (Future<?> task : list) {
+            ExtractableResponse<Response> response = (ExtractableResponse<Response>) task.get();
+            System.out.println("res: " + response.statusCode());
+            System.out.println("res: " + response.asString());
+            if (response.statusCode() == 500 &&
+                "이미 재고가 차감되었습니다.".equals(response.asString())) {
+                failedCount++;
+            }
+        }
+
+        assertThat(failedCount).isEqualTo(3);
     }
 
     ExtractableResponse<Response> N개의_재고를_생성(Long stockCount) {
-        StockRequest stockRequest = new StockRequest("돈까스", "T-1101", stockCount);
+        StockRequest stockRequest = new StockRequest("돈까스", stockCount);
 
         return RestAssured
             .given().log().all()
@@ -66,9 +74,8 @@ public class StockAcceptanceTest {
             .given().log().all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .param("pickingCount", pickingCount)
-            .when().put("/stocks/{stockId}/picking", stockId)
+            .when().put("/stocks/{stockId}/decrease", stockId)
             .then().log().all()
             .extract();
-
     }
 }
