f456e75253f1196a2374138ef57dfd71aa49628c
limwoobin <drogba02@naver.com>
2022-07-30 22:50:30 +0900

    feat: ImmutableObj example add

---
+++ java-tech-example/src/main/java/immutableObject/ImmutableObj.java
@@ -0,0 +1,13 @@
+package immutableObject;
+
+public final class ImmutableObj {
+    private final int num;
+
+    public ImmutableObj(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
