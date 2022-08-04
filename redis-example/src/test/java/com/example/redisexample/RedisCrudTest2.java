3eea970e7c6cf40f0b7b615c3b62e19b12245e7b
woobeen <drogba02@naver.com>
2022-08-05 02:32:40 +0900

    test: redis test code BDD 형식 주석 추가

--- redis-example/src/test/java/com/example/redisexample/RedisCrudTest2.java
+++ redis-example/src/test/java/com/example/redisexample/RedisCrudTest2.java
@@ -40,11 +40,14 @@ class RedisCrudTest2 {
     @Test
     @DisplayName("Redis 에 데이터를 저장하면 정상적으로 조회되어야 한다")
     void redis_save_test() {
+        // given
         productRepository.save(product);
 
+        // when
         Product persistProduct = productRepository.findById(product.getId())
                 .orElseThrow(RuntimeException::new);
 
+        // then
         assertThat(persistProduct.getId()).isEqualTo(product.getId());
         assertThat(persistProduct.getName()).isEqualTo(product.getName());
         assertThat(persistProduct.getPrice()).isEqualTo(product.getPrice());
@@ -53,26 +56,30 @@ class RedisCrudTest2 {
     @Test
     @DisplayName("Redis 에 데이터를 수정하면 정상적으로 수정되어야 한다")
     void redis_update_test() {
+        // given
         productRepository.save(product);
-
         Product persistProduct = productRepository.findById(product.getId())
                 .orElseThrow(RuntimeException::new);
 
+        // when
         persistProduct.changePrice(35000L);
         productRepository.save(persistProduct);
 
+        // then
         assertThat(persistProduct.getPrice()).isEqualTo(35000L);
     }
 
     @Test
     @DisplayName("Redis 에 데이터를 삭제하면 정상적으로 삭제되어야 한다")
     void redis_delete_test() {
+        // given
         productRepository.save(product);
 
+        // when
         productRepository.delete(product);
-
         Optional<Product> deletedProduct = productRepository.findById(product.getId());
 
+        // then
         assertTrue(deletedProduct.isEmpty());
     }
 }
