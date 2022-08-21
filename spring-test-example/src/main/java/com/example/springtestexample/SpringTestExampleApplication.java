81b1b10ce6ebe4e5428156732c24ac5d5faadc37
limwoobin <drogba02@naver.com>
2022-08-21 17:08:46 +0900

    test example initial commit

---
+++ spring-test-example/src/main/java/com/example/springtestexample/SpringTestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestExampleApplication.class, args);
+    }
+
+}
---
+++ spring-test-example/src/test/java/com/example/springtestexample/SpringTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
