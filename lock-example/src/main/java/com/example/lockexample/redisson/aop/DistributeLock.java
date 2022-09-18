56530fa377ec157517f0b3e8132d1de050b4b23d
woobeen <drogba02@naver.com>
2022-09-18 20:49:51 +0900

    feat: redisson DistributeLock aop 추가

--- lock-example/src/main/java/com/example/lockexample/redisson/aop/DistributeLock.java
+++ lock-example/src/main/java/com/example/lockexample/redisson/aop/DistributeLock.java
@@ -4,9 +4,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DistributeLock {
     String key();
+
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    long waitTime() default 4L;
+
+    long releaseTime() default 3L;
 }
--- lock-example/src/main/java/com/example/lockexample/redisson/aop/DistributeLockAop.java
+++ lock-example/src/main/java/com/example/lockexample/redisson/aop/DistributeLockAop.java
@@ -1,13 +1,20 @@
 package com.example.lockexample.redisson.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
 @Aspect
 @Component
+@Slf4j
 public class DistributeLockAop {
     private static String REDISSON_KEY_PREFIX = "RLOCK_";
 
@@ -19,6 +26,30 @@ public class DistributeLockAop {
 
     @Around("@annotation(com.example.lockexample.redisson.aop.DistributeLock)")
     public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributeLock distributeLock = method.getAnnotation(DistributeLock.class);
+
+        String key = REDISSON_KEY_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributeLock.key());
+        log.info("lockKey {}" , key);
+
+        RLock rLock = redissonClient.getLock(key);
+        TimeUnit timeUnit = (TimeUnit) CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), "timeUnit");
+
+        try {
+            boolean available = rLock.tryLock(4L, 3L, timeUnit);
+            if (!available) {
+                return false;
+            }
+
+            return joinPoint.proceed();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        } finally {
+            rLock.unlock();
+        }
+
         return joinPoint.proceed();
     }
 }
