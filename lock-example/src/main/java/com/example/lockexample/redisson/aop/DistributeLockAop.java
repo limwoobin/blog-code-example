6d49871daf9c3e77d31c11bee85de70f5dcb1d30
woobeen <drogba02@naver.com>
2022-09-23 19:37:45 +0900

    refactor: aop transaction 선언 변경 (클래스 분리)

--- lock-example/src/main/java/com/example/lockexample/redisson/aop/DistributeLockAop.java
+++ lock-example/src/main/java/com/example/lockexample/redisson/aop/DistributeLockAop.java
@@ -9,10 +9,6 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.lang.reflect.Method;
 
@@ -24,7 +20,7 @@ public class DistributeLockAop {
     private static final String REDISSON_KEY_PREFIX = "RLOCK_";
 
     private final RedissonClient redissonClient;
-    private final PlatformTransactionManager transactionManager;
+    private final AopForTransaction aopForTransaction;
 
 
     @Around("@annotation(com.example.lockexample.redisson.aop.DistributeLock)")
@@ -36,7 +32,6 @@ public class DistributeLockAop {
         String key = REDISSON_KEY_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributeLock.key());
 
         RLock rLock = redissonClient.getLock(key);
-        TransactionStatus status = null;
 
         try {
             boolean available = rLock.tryLock(distributeLock.waitTime(), distributeLock.leaseTime(), distributeLock.timeUnit());
@@ -45,18 +40,9 @@ public class DistributeLockAop {
             }
 
             log.info("get lock success {}" , key);
-            DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
-            transactionDefinition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-
-            status = transactionManager.getTransaction(transactionDefinition);
-
-            Object result = joinPoint.proceed();
-            transactionManager.commit(status);
-            return result;
+            return aopForTransaction.proceed(joinPoint);
         } catch (Exception e) {
             Thread.currentThread().interrupt();
-            System.out.println("status ###" + status);
-            transactionManager.rollback(status);
             throw new InterruptedException();
         } finally {
             log.info("unLocked ######");
