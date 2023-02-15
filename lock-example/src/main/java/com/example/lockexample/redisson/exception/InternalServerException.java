e86c27aafe13d6342bea4c39273a4a658b38c832
woobeen <drogba02@naver.com>
2023-02-15 21:21:01 +0900

    feat: 커스텀 예외 메시지를 위한 생성자 추가

--- lock-example/src/main/java/com/example/lockexample/redisson/exception/InternalServerException.java
+++ lock-example/src/main/java/com/example/lockexample/redisson/exception/InternalServerException.java
@@ -1,4 +1,8 @@
 package com.example.lockexample.redisson.exception;
 
 public class InternalServerException extends RuntimeException {
+
+    public InternalServerException(String message) {
+        super(message);
+    }
 }
