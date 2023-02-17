794b2fad40cab3ea075305036c8bbf43f6fdca80
woobeen <drogba02@naver.com>
2023-02-17 20:10:03 +0900

    refactor: 분산락 기본예외 String 생성자 추가 (예외 skip 전용)

--- lock-example/src/main/java/com/example/lockexample/redisson/exception/ExceptionIgnore.java
+++ lock-example/src/main/java/com/example/lockexample/redisson/exception/ExceptionIgnore.java
@@ -1,4 +1,12 @@
 package com.example.lockexample.redisson.exception;
 
 public class ExceptionIgnore extends Exception {
+
+    /**
+     * 분산락 커스텀 예외를 무시하기 위한 생성자
+     * @param s
+     */
+    public ExceptionIgnore(String s) {
+        super(s);
+    }
 }
