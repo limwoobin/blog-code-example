0def92ee2e49d438268a327ceb5d2ff43a30cb9b
limwoobin <drogba02@naver.com>
2023-10-24 19:54:29 +0900

    test: 테스트 리스너 라이프사이클 테스트

---
+++ spring-test-context/src/test/java/com/example/springtestcontext/CustomTestExecutionListener.java
@@ -0,0 +1,29 @@
+package com.example.springtestcontext;
+
+import com.example.springtestcontext.domain.Coupon;
+import com.example.springtestcontext.domain.CouponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
+
+public class CustomTestExecutionListener implements TestExecutionListener {
+  private static boolean initialize = false;
+
+  @Autowired
+  private CouponRepository couponRepository;
+
+  @Override
+  public void prepareTestInstance(TestContext testContext) throws Exception {
+    testContext.getApplicationContext()
+      .getAutowireCapableBeanFactory()
+      .autowireBean(this);
+
+    if (!initialize) {
+      Coupon coupon = new Coupon(1L, "coupon");
+      couponRepository.save(coupon);
+
+      System.out.println("Test Prepare");
+      initialize = true;
+    }
+  }
+}
