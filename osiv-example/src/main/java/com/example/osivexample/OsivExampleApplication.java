8aea806ae795acd29b47271b77d9b2e2533ba8ce
limwoobin <drogba02@naver.com>
2023-11-20 21:04:13 +0900

    feat: Spring osiv project example init

---
+++ osiv-example/src/main/java/com/example/osivexample/OsivExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.osivexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsivExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OsivExampleApplication.class, args);
+  }
+
+}
---
+++ osiv-example/src/test/java/com/example/osivexample/OsivExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osivexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsivExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
