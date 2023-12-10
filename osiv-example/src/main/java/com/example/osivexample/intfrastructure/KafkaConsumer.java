3149707494661a0a9dce896165ff33784ce509cf
limwoobin <drogba02@naver.com>
2023-12-11 01:42:54 +0900

    feat: osiv example project add

--- osiv-example/src/main/java/com/example/osivexample/intfrastructure/KafkaConsumer.java
+++ osiv-example/src/main/java/com/example/osivexample/intfrastructure/KafkaConsumer.java
@@ -16,15 +16,15 @@ public class KafkaConsumer {
 
   private final TeamService teamService;
 
-  @KafkaListener(
-    topics = "${spring.kafka.topics.osiv-test}",
-    groupId = "${spring.kafka.consumer.group-id}",
-    containerFactory = "containerFactory"
-  )
-  public void consume(@Payload Long id, Acknowledgment ack) {
-    Team team = teamService.findTeam(id);
-
-    log.info("member size = {}", team.getMembers().size());
-    ack.acknowledge();
-  }
+//  @KafkaListener(
+//    topics = "${spring.kafka.topics.osiv-test}",
+//    groupId = "${spring.kafka.consumer.group-id}",
+//    containerFactory = "containerFactory"
+//  )
+//  public void consume(@Payload Long id, Acknowledgment ack) {
+//    Team team = teamService.findTeam(id);
+//
+//    log.info("member size = {}", team.getMembers().size());
+//    ack.acknowledge();
+//  }
 }
