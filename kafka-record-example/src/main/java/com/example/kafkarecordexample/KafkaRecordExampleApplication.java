9f8f7426a7aaf79d955f5399cd082e2b9769803e
limwoobin <drogba02@naver.com>
2023-12-17 16:24:19 +0900

    feat: Spring Kafka Record example init

---
+++ kafka-record-example/src/main/java/com/example/kafkarecordexample/KafkaRecordExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkarecordexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRecordExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRecordExampleApplication.class, args);
+	}
+
+}
