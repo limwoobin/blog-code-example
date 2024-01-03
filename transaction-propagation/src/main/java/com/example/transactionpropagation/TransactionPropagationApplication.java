498d323013c177075a30156a453b0d3bd4ac78ca
limwoobin <drogba02@naver.com>
2024-01-03 20:01:52 +0900

    feat: transaction propagation project init

---
+++ transaction-propagation/src/main/java/com/example/transactionpropagation/TransactionPropagationApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionpropagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionPropagationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransactionPropagationApplication.class, args);
+  }
+
+}
---
+++ transaction-propagation/src/test/java/com/example/transactionpropagation/TransactionPropagationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionpropagation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionPropagationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
