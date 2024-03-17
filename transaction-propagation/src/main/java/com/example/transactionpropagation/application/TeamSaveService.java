075a6598c97b50dcdf14cecb5570d864c756e4ed
limwoobin <drogba02@naver.com>
2024-03-17 17:16:03 +0900

    feat: 트랜잭션 rollback 테스 v2

--- transaction-propagation/src/main/java/com/example/transactionpropagation/application/TeamSaveService.java
+++ transaction-propagation/src/main/java/com/example/transactionpropagation/application/TeamSaveService.java
@@ -5,14 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Component
 @RequiredArgsConstructor
 public class TeamSaveService {
   private final TeamRepository teamRepository;
 
-  @Transactional(propagation = Propagation.REQUIRES_NEW)
+//  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  @Transactional
   public void save(String name) {
+    boolean transactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+    String transactionName = TransactionSynchronizationManager.getCurrentTransactionName();
+    System.out.println("transactionActive: " + transactionActive);
+    System.out.println("transactionName: " + transactionName);
+
     throw new RuntimeException();
   }
 }
--- transaction-propagation/src/main/java/com/example/transactionpropagation/application/TeamServiceV2.java
+++ transaction-propagation/src/main/java/com/example/transactionpropagation/application/TeamServiceV2.java
@@ -5,6 +5,7 @@ import com.example.transactionpropagation.domain.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,11 @@ public class TeamServiceV2 {
 //  @Transactional(rollbackFor = Throwable.class)
   @Transactional
   public void save(String name) {
+    boolean transactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+    String transactionName = TransactionSynchronizationManager.getCurrentTransactionName();
+    System.out.println("transactionActive: " + transactionActive);
+    System.out.println("transactionName: " + transactionName);
+
     try {
       Team team = Team.from(name);
       teamRepository.save(team);
