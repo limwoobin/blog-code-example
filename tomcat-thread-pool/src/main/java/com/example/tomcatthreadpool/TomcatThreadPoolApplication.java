37dd765719b85692417376b53b0fbd11d4bf3304
limwoobin <drogba02@naver.com>
2024-07-28 17:37:10 +0900

    feat: Tomcat Thread Pool exam code init

---
+++ tomcat-thread-pool/src/main/java/com/example/tomcatthreadpool/TomcatThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcatthreadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatThreadPoolApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TomcatThreadPoolApplication.class, args);
+  }
+
+}
