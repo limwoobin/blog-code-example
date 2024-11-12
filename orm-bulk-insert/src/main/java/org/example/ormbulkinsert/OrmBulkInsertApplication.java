724988132151e7f4ac98531bc1b2f9537f6c5a26
limwoobin <drogba02@naver.com>
2024-11-12 21:51:48 +0900

    feat: orm bulk insert init

---
+++ orm-bulk-insert/src/main/java/org/example/ormbulkinsert/OrmBulkInsertApplication.java
@@ -0,0 +1,13 @@
+package org.example.ormbulkinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmBulkInsertApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrmBulkInsertApplication.class, args);
+  }
+
+}
---
+++ orm-bulk-insert/src/test/java/org/example/ormbulkinsert/OrmBulkInsertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ormbulkinsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmBulkInsertApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
